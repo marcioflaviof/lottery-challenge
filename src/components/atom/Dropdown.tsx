4301d78728bf98671c93776a0dae77e231811e6a
Marcio Flavio <mflaviof1995@gmail.com>
2021-07-07 18:16:10 -0300

    Add dropdown component

---
+++ src/components/atom/Dropdown.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Dropdown = () => {
+  return (
+    <select className="dropdown">
+      <option value="">MEGA-SENA</option>
+      <option value="0">QUINA</option>
+      <option value="1">LOTOFACIL</option>
+      <option value="2">LOTOMANIA</option>
+      <option value="3">TIMEMANIA</option>
+      <option value="4">DIA DE SORTE</option>
+    </select>
+  );
+};
+
+export default Dropdown;
