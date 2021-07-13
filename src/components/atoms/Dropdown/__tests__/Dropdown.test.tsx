1007cce9e8da18b74f822e2ce4b45066d1769c92
Marcio Flavio <mflaviof1995@gmail.com>
2021-07-13 09:22:41 -0300

    Add tests to another options

--- src/components/atoms/Dropdown/__tests__/Dropdown.test.tsx
+++ src/components/atoms/Dropdown/__tests__/Dropdown.test.tsx
@@ -13,11 +13,56 @@ describe("Dropdown", () => {
       expect(getByRole("combobox").childElementCount).toStrictEqual(6);
     });
 
+    it("must have MEGA-SENA", () => {
+      const { getByRole } = render(<Dropdown />);
+
+      expect(getByRole("option", { name: /mega-sena/i })).toBeInTheDocument();
+      expect(getByRole("option", { name: /mega-sena/i }).firstChild?.nodeValue).toStrictEqual(
+        "MEGA-SENA"
+      );
+    });
+
     it("must have QUINA", () => {
       const { getByRole } = render(<Dropdown />);
 
       expect(getByRole("option", { name: /quina/i })).toBeInTheDocument();
       expect(getByRole("option", { name: /quina/i }).firstChild?.nodeValue).toStrictEqual("QUINA");
+    });
+
+    it("must have LOTOFACIL", () => {
+      const { getByRole } = render(<Dropdown />);
+
+      expect(getByRole("option", { name: /lotofacil/i })).toBeInTheDocument();
+      expect(getByRole("option", { name: /lotofacil/i }).firstChild?.nodeValue).toStrictEqual(
+        "LOTOFACIL"
+      );
+    });
+
+    it("must have LOTOMANIA", () => {
+      const { getByRole } = render(<Dropdown />);
+
+      expect(getByRole("option", { name: /lotomania/i })).toBeInTheDocument();
+      expect(getByRole("option", { name: /lotomania/i }).firstChild?.nodeValue).toStrictEqual(
+        "LOTOMANIA"
+      );
+    });
+
+    it("must have TIMEMANIA", () => {
+      const { getByRole } = render(<Dropdown />);
+
+      expect(getByRole("option", { name: /timemania/i })).toBeInTheDocument();
+      expect(getByRole("option", { name: /timemania/i }).firstChild?.nodeValue).toStrictEqual(
+        "TIMEMANIA"
+      );
+    });
+
+    it("must have DIA DE SORTE", () => {
+      const { getByRole } = render(<Dropdown />);
+
+      expect(getByRole("option", { name: /dia de sorte/i })).toBeInTheDocument();
+      expect(getByRole("option", { name: /dia de sorte/i }).firstChild?.nodeValue).toStrictEqual(
+        "DIA DE SORTE"
+      );
     });
   });
 });
