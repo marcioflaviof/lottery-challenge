51b7c7699ced8e5b8bd4a45f475f8af461db6fac
Marcio Flavio <mflaviof1995@gmail.com>
2021-07-19 11:56:17 -0300

    Fixing Circle test

--- src/components/atoms/Circle/__tests__/Circle.test.tsx
+++ src/components/atoms/Circle/__tests__/Circle.test.tsx
@@ -1,17 +1,14 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
-import React from "react";
 import { renderComponentCreator } from "../../../utils/component-creator/ComponentCreator";
 import Circle from "../Circle";
 
 describe("Circle", () => {
-  const renderComponent = renderComponentCreator(Circle, () => ({}));
+  const renderComponent = renderComponentCreator(Circle);
 
   describe("when calls with correct arguments", () => {
     it("must render with passed number", () => {
-      const { getByText, debug, container } = renderComponent({ number: 6 });
-
-      debug(container);
+      const { getByText } = renderComponent({ number: 5 });
 
       expect(getByText(/5/i)).toBeInTheDocument();
     });
