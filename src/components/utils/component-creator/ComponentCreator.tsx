f2027ffa04f6e48e94c8c6e726f7c24daaafad2e
Marcio Flavio <mflaviof1995@gmail.com>
2021-07-21 09:26:51 -0300

    Changing new props to optative and any is not more allowed

--- src/components/utils/component-creator/ComponentCreator.tsx
+++ src/components/utils/component-creator/ComponentCreator.tsx
@@ -3,11 +3,9 @@ import React from "react";
 
 type Return = (newProps: Record<string, unknown>) => RenderResult;
 
-const renderComponentCreator = (
-  Component: React.FC<any>,
-  propsFn = () => ({})
-): Return => {
-  return (newProps: Record<string, unknown>) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const renderComponentCreator = (Component: React.FC<any>, propsFn = () => ({})): Return => {
+  return (newProps?: Record<string, unknown>) => {
     const props = { ...propsFn(), ...newProps };
 
     return {
