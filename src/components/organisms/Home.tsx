0c43191f273e4ec73106f9bdc2097cbce7ce7acc
Marcio Flavio <mflaviof1995@gmail.com>
2021-08-16 21:51:34 -0300

    Finishing homepage

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -1,3 +1,4 @@
+import Dropdown from "../atoms/Dropdown/Dropdown";
 import Footer from "../atoms/Footer/Footer";
 import Results from "../molecules/Results/Results";
 
@@ -7,7 +8,9 @@ const Home = (): JSX.Element => {
   return (
     <>
       <div className="App__container">
-        <div className="App__navigation"></div>
+        <div className="App__navigation">
+          <Dropdown />
+        </div>
         <div className="App__content">
           <Results circles={[10, 23, 45, 68, 70, 55]} />
           <Footer />
