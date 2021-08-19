536e6ee72e905e7bd146e1cd46beac51edafb7f7
Marcio Flavio <mflaviof1995@gmail.com>
2021-08-19 11:34:46 -0300

    Showing API data Getting api data in useLottery custom hook and exporting to components who needs. (still needs tests and better way to share components)

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -1,21 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useLottery from "../../hooks/useLottery";
 import Dropdown from "../atoms/Dropdown/Dropdown";
 import Footer from "../atoms/Footer/Footer";
 import { Icon } from "../atoms/Icons/Icons";
 import Results from "../molecules/Results/Results";
+import { Option } from "../atoms/Dropdown/Dropdown";
 
 import "./style.css";
 
 const Home = (): JSX.Element => {
+  const [options, setOptions] = useState<Option[]>([]);
   const [results, setResults] = useState<string[]>([]);
-  const { getResult, options } = useLottery();
+  const { getLotteries, getResult } = useLottery();
 
   const onOptionChange = async (selectedOption: string) => {
     const result = await getResult(selectedOption);
 
     setResults(result.numbers);
   };
+
+  useEffect(() => {
+    const lotteries = async () =>
+      await getLotteries()
+        .then((lotteries) => {
+          return lotteries.map((lottery) => {
+            return {
+              id: lottery.id,
+              text: lottery.name.toUpperCase(),
+            } as Option;
+          });
+        })
+        .then((option) => setOptions(option));
+
+    void lotteries();
+  }, []);
 
   return (
     <>
