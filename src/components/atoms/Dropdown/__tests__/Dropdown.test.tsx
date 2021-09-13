f6e2edf453491d2c7ef3bc6eeec21c5ee2976742
Marcio Flavio <mflaviof1995@gmail.com>
2021-09-13 15:02:06 -0300

    Update tests (#9)
    * Update tests
    - Create new tests for useLottery hook
    - Import nock to mock api connections
    - Update types of typescript to fit axios adapter

--- src/components/atoms/Dropdown/__tests__/Dropdown.test.tsx
+++ src/components/atoms/Dropdown/__tests__/Dropdown.test.tsx
@@ -1,68 +1,29 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
-import React from "react";
+import { renderComponentCreator } from "../../../utils/component-creator/ComponentCreator";
 import Dropdown from "../Dropdown";
 
 describe("Dropdown", () => {
-  describe("when calls", () => {
-    it("must render with 6 options", () => {
-      const { getByRole } = render(<Dropdown />);
+  const renderComponent = renderComponentCreator(Dropdown, () => ({
+    options: [
+      { id: 1, text: "mega-sena" },
+      { id: 3, text: "quina" },
+    ],
+    onChange: () => {},
+  }));
+
+  describe("when pass two options", () => {
+    it("must render with two options", () => {
+      const { getByRole } = renderComponent({});
 
       expect(getByRole("combobox")).toBeInTheDocument();
-      expect(getByRole("combobox").childElementCount).toStrictEqual(6);
+      expect(getByRole("combobox").childElementCount).toStrictEqual(2);
     });
 
-    it("must have MEGA-SENA", () => {
-      const { getByRole } = render(<Dropdown />);
+    it("must render the text", () => {
+      const { getByRole } = renderComponent({});
 
-      expect(getByRole("option", { name: /mega-sena/i })).toBeInTheDocument();
-      expect(getByRole("option", { name: /mega-sena/i }).firstChild?.nodeValue).toStrictEqual(
-        "MEGA-SENA"
-      );
-    });
-
-    it("must have QUINA", () => {
-      const { getByRole } = render(<Dropdown />);
-
-      expect(getByRole("option", { name: /quina/i })).toBeInTheDocument();
-      expect(getByRole("option", { name: /quina/i }).firstChild?.nodeValue).toStrictEqual("QUINA");
-    });
-
-    it("must have LOTOFACIL", () => {
-      const { getByRole } = render(<Dropdown />);
-
-      expect(getByRole("option", { name: /lotofacil/i })).toBeInTheDocument();
-      expect(getByRole("option", { name: /lotofacil/i }).firstChild?.nodeValue).toStrictEqual(
-        "LOTOFACIL"
-      );
-    });
-
-    it("must have LOTOMANIA", () => {
-      const { getByRole } = render(<Dropdown />);
-
-      expect(getByRole("option", { name: /lotomania/i })).toBeInTheDocument();
-      expect(getByRole("option", { name: /lotomania/i }).firstChild?.nodeValue).toStrictEqual(
-        "LOTOMANIA"
-      );
-    });
-
-    it("must have TIMEMANIA", () => {
-      const { getByRole } = render(<Dropdown />);
-
-      expect(getByRole("option", { name: /timemania/i })).toBeInTheDocument();
-      expect(getByRole("option", { name: /timemania/i }).firstChild?.nodeValue).toStrictEqual(
-        "TIMEMANIA"
-      );
-    });
-
-    it("must have DIA DE SORTE", () => {
-      const { getByRole } = render(<Dropdown />);
-
-      expect(getByRole("option", { name: /dia de sorte/i })).toBeInTheDocument();
-      expect(getByRole("option", { name: /dia de sorte/i }).firstChild?.nodeValue).toStrictEqual(
-        "DIA DE SORTE"
-      );
+      expect(getByRole("option", { name: /mega-sena/ })).toBeInTheDocument();
     });
   });
 });
--- src/components/molecules/Results/__tests__/Results.test.tsx
+++ src/components/molecules/Results/__tests__/Results.test.tsx
@@ -7,19 +7,13 @@ describe("Results", () => {
   const renderComponent = renderComponentCreator(Results);
 
   describe("when pass an array of numbers", () => {
-    it("must render every number in a circle", () => {
-      const { getByTestId, getByText } = renderComponent({ results: [1, 3, 5, 7, 8, 54] });
+    it("must render every number", () => {
+      const { getByTestId, getByText } = renderComponent({
+        circles: ["1", "3", "5", "7", "8", "54"],
+      });
 
       expect(getByText(/7/i)).toBeInTheDocument();
       expect(getByTestId(/circle-results/i).childElementCount).toBe(6);
-    });
-  });
-
-  describe("when pass strings in the array", () => {
-    it("won't render the strings", () => {
-      const { queryByText } = renderComponent({ results: ["8", 2, 3, 5] });
-
-      expect(queryByText(/8/i)).not.toBeInTheDocument();
     });
   });
 });
